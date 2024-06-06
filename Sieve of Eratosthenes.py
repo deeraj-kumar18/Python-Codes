efc4508e768941ef03ee40283496c5736850c599
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-06-06 11:54:34 +0530

    Completed code

--- Sieve of Eratosthenes.py
+++ Sieve of Eratosthenes.py
@@ -7,5 +7,21 @@ ALGORITHM:
 4)The remaining numbers are Prime.
 """
 def sieve_of_eratosthenes(n):
-    
-    return
+    # Initialsing list of Boolean values with True
+    is_primes=[True]*(n+1)
+    is_primes[0]=is_primes[1]=False  # 0 and 1 are not primes.
+
+    # marking multiples of a number from 2 to sq root of n
+    for i in range(2,int(n**0.5)+1):
+        if is_primes[i]==True: 
+            for j in range(i*i,n+1,i): #  Why i**2? All multiples of i less than i**2 are already marked by primes smaller than i.
+                is_primes[j]=False
+
+
+    prime_nos=[num for num in range(len(is_primes)) if is_primes[num]==True ]
+
+    return prime_nos
+
+n=int(input("Enter the number to find the Prime numbers till that range: "))
+primes=(sieve_of_eratosthenes(n))
+print(f"The List of Prime numbers in the given range are: {primes}")

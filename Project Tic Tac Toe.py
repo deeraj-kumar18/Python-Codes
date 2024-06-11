2dcc4f59d13df86afd44681b94e370c2f42f3b94
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-06-12 00:19:40 +0530

    all functions except play_game

--- Project Tic Tac Toe.py
+++ Project Tic Tac Toe.py
@@ -17,5 +17,64 @@ def print_grid(grid):
         
 def get_user_input():
     row,col=map(int,input("Enter the row and col number of the grid, where you want to mark it. ").split())
-    print(row,col)
+    return row,col
 
+def place_mark(sym,row,col,grid):
+    grid[row][col]=sym
+    
+def check_win(grid):
+    all_lines=vertical_check(grid)+horizontal_check(grid)+diagonal_check(grid)
+
+    player_x = ["X"]*len(grid)
+    player_o = ["O"]*len(grid)
+
+    if player_x  in all_lines:
+        return "Player X wins."
+    elif player_o in all_lines:
+        return "Player O wins."
+
+def vertical_check(grid):
+    cols=[]
+    for i in range(len(grid)):
+        col=[]
+        for row in range(len(grid)):
+            col.append(grid[row][i])
+        cols.append(col)
+    return cols
+
+def horizontal_check(grid):
+    rows=[]
+    for row in grid:
+        rows.append(list(row))
+    return rows
+
+def diagonal_check(grid):  
+    diag=[]
+    left_right_diag=[]
+    for i in range(len(grid)):
+        left_right_diag.append(grid[i][i])
+    
+    right_left_diag=[]
+    for i in range(len(grid)):
+            right_left_diag.append(grid[i][len(grid)-i-1])
+
+    diag.append(left_right_diag)
+    diag.append(right_left_diag)
+    return diag
+
+def check_draw(grid):
+    return
+
+def switch_player(player):
+    if player=="p1":
+        return "p2"
+    else:
+        return "p1"
+
+def play_game():
+    while True:
+        
+        grid=initialize_grid()
+# b=[["X","e","X"],["W","W","z"],["O","O","O"]]
+# a=check_win(b)
+# print(a)

1ba5796f15bf487e45c0aafbc6226ad8f7abaedd
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-07-30 10:53:26 +0530

    pattern 4

--- Patterns.py
+++ Patterns.py
@@ -36,11 +36,24 @@ def pattern3(n):
             print(j,end=" ")
         print()
 
-        
+# Pattern 4
+'''
+1 
+2 2 
+3 3 3
+'''   
+
+def pattern4(n):
+    print("Pattern 4")
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            print(i, end = " ")
+        print()
 
 def main():
     pattern1(3)
     pattern2(3)
     pattern3(3)
+    pattern4(3)
 
 main()

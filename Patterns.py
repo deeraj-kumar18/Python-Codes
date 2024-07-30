302d64e51dcea63d9fa4a1cdfaa666a39ab1e564
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-07-30 10:44:18 +0530

    pattern 2

--- Patterns.py
+++ Patterns.py
@@ -6,10 +6,26 @@
 '''
 
 def pattern1(n):
+    print("Pattern 1")
     for i in range(n):
         print("* "*n)
 
+# Pattern 2
+'''
+* 
+* *
+* * *
+'''
+
+def pattern2(n):
+    print("Pattern 2")
+    for i in range(n+1):
+        print("* "*i)
+
+
+
 def main():
     pattern1(3)
+    pattern2(3)
 
 main()

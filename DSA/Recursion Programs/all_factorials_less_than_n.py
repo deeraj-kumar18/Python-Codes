6e711a126a6923ccabf34134942e3f3f2202494a
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-08-01 22:16:59 +0530

    all factorials less than n

---
+++ DSA/Recursion Programs/all_factorials_less_than_n.py
@@ -0,0 +1,28 @@
+'''
+Find all factorial numbers less than or equal to n
+Difficulty: EasyAccuracy: 48.96%Submissions: 61K+Points: 2
+A number n is called a factorial number if it is the factorial of a positive integer. For example, the first few factorial numbers are 1, 2, 6, 24, 120,
+Given a number n, the task is to return the list/vector of the factorial numbers smaller than or equal to n.
+
+Examples:
+
+Input: n = 3
+Output: 1 2
+Explanation: The first factorial number is 1 which is less than equal to n. The second number is 2 which is less than equal to n,but the third factorial number is 6 which is greater than n. So we print only 1 and 2.
+Input: n = 6
+Output: 1 2 6
+'''
+def factorialNumbers(n):
+    def findFactorial(cur_Fact,i):
+        if cur_Fact>n:
+            return  
+        result.append(cur_Fact)
+        findFactorial(cur_Fact * (i+1),i+1)
+    
+
+    result=[]
+    findFactorial(1,1)
+    return result
+
+for i in range(1,500):
+    print(factorialNumbers(i))

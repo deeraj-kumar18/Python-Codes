ddae452f0507013090fdf1ab6adf3405ee4171be
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-08-06 15:14:52 +0530

    quick sort func def

--- DSA/Sorting/quick_sort.py
+++ DSA/Sorting/quick_sort.py
@@ -1,7 +1,9 @@
 '''
 QUICK SORT 
-
+Principle: Pick a pivot and place it in its correct position.
 Time Complexity: O(N logN)
 Space Complexity: O(1) 
 Why Quick Sort over Merge sort? It doesnt use extra space. 
 '''
+def quick_sort(arr,n):
+    return

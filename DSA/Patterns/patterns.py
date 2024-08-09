18b0d6573cb15c32290cc500fc6a81687e1c7a0e
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-08-09 18:39:18 +0530

    pattern 3 code

--- DSA/Patterns/patterns.py
+++ DSA/Patterns/patterns.py
@@ -26,7 +26,13 @@ def pattern2(n):
 1 2 
 1 2 3
 '''
-
+def pattern3(n):
+    print("Pattern 3")
+    for i in range(1,n+1):
+        for j in range(1,i+1):
+            print(j,end=" ")
+        print()
+        
 def main():
     pattern1(3)
     pattern2(3)

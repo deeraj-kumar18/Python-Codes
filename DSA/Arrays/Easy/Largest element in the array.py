ce5a570bd6f84e3ce7d29eede9e4b3913d446b5a
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-08-10 16:47:22 +0530

    testing of largest element code

--- DSA/Arrays/Easy/Largest element in the array.py
+++ DSA/Arrays/Easy/Largest element in the array.py
@@ -2,7 +2,7 @@
 Largest Element in Array
 Given an array arr, the task is to find the largest element in it.
 '''
-def largest(n ,arr):
+def largest(arr):
         # code here
         maxi=arr[0]
         for i in range(len(arr)):
@@ -10,3 +10,10 @@ def largest(n ,arr):
                 maxi=arr[i]
                 
         return maxi
+# Testing
+testcases= [[1, 8, 7, 56, 90],[5, 5, 5, 5],[10]]
+def main():
+     for testcase in testcases:
+          print(largest(testcase))
+
+main()

1b9e98b60daec800f8fef230f20aa2c77494265b
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-08-10 16:44:15 +0530

    largest ele code

--- DSA/Arrays/Easy/Largest element in the array.py
+++ DSA/Arrays/Easy/Largest element in the array.py
@@ -2,3 +2,11 @@
 Largest Element in Array
 Given an array arr, the task is to find the largest element in it.
 '''
+def largest(n ,arr):
+        # code here
+        maxi=arr[0]
+        for i in range(len(arr)):
+            if arr[i]>=maxi:
+                maxi=arr[i]
+                
+        return maxi

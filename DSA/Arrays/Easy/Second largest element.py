7259f64147a27a44cdb27b9b34e8c0a6ebeb91dd
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-08-11 22:11:35 +0530

    brute force approach, second largest element

--- DSA/Arrays/Easy/Second largest element.py
+++ DSA/Arrays/Easy/Second largest element.py
@@ -3,3 +3,11 @@ Second Largest
 Given an array arr, return the second largest distinct element from an array.
  If the second largest element doesn't exist then return -1.
 '''
+# BRUTE FORCE APPROACH
+def second_largest(arr):
+    a=list(set(arr))
+    a.sort()
+    return a[-2]
+
+arr=[1,7,8,6,4,2]
+print(second_largest(arr))

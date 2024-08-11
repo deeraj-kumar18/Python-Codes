a24444c7c19061492bbd72cedd20fdf351dd8e64
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-08-11 22:51:30 +0530

    second largest ques, Better approach

--- DSA/Arrays/Easy/Second largest element.py
+++ DSA/Arrays/Easy/Second largest element.py
@@ -3,11 +3,26 @@ Second Largest
 Given an array arr, return the second largest distinct element from an array.
  If the second largest element doesn't exist then return -1.
 '''
-# BRUTE FORCE APPROACH
-def second_largest(arr):
+# BRUTE FORCE APPROACH Time Complexity : O(NlogN)
+def second_largest_bruteforce(arr):
     a=list(set(arr))
     a.sort()
     return a[-2]
 
+#  BETTER APPROACH  Time Complexity : O(2N)
+def second_largest_better(arr): 
+    largest=arr[0]
+    for i in range(len(arr)):
+        if(arr[i]>=largest):
+            largest=arr[i]
+
+    second_largest=float('-inf')  # If there are Negative numbers.
+    for i in range(len(arr)):
+        if(arr[i]>second_largest and arr[i]!=largest):
+            second_largest=arr[i]
+
+    return second_largest
+
 arr=[1,7,8,6,4,2]
-print(second_largest(arr))
+print(second_largest_bruteforce(arr))
+print(second_largest_better(arr))

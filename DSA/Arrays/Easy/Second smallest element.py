d110f2f496d8aabe62b9ebda0e8d0061ee95a90c
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-08-11 23:51:44 +0530

    second smallest bruteforce approach

--- DSA/Arrays/Easy/Second smallest element.py
+++ DSA/Arrays/Easy/Second smallest element.py
@@ -3,3 +3,13 @@ Second Smallest
 Given an array arr, return the second smallest distinct element from an array.
  If the second smallest element doesn't exist then return -1.
 '''
+# BRUTEFORCE APPROACH
+def second_smallest_bruteforce(arr):
+    a=list(set(arr))
+    a.sort()
+    if len(a)==1:
+        return a[0]
+    return a[1]
+
+arr=[12, 35, 1, 10, 34, 1] 
+print(second_smallest_bruteforce(arr))

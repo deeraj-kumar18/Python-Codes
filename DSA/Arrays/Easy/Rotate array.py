e9241038523b7aab5f0cdab66021f10473df1543
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-08-12 23:32:41 +0530

    tested on multiple testcases.

--- DSA/Arrays/Easy/Rotate array.py
+++ DSA/Arrays/Easy/Rotate array.py
@@ -24,6 +24,22 @@ def rotate_array(arr,k):
     return arr
 
 # Initial testing
-nums = [1,2,3,4,5,6,7]
-k = 3
-print(rotate_array(nums,k))
+# nums = [1,2,3,4,5,6,7]
+# k = 3
+# print(rotate_array(nums,k))
+
+# Testing
+test_cases = [
+    ([1, 2, 3, 4, 5, 6, 7], 3),   # Expected Output: [5, 6, 7, 1, 2, 3, 4]
+    ([-1, -100, 3, 99], 2),       # Expected Output: [3, 99, -1, -100]
+    ([1, 2, 3, 4, 5, 6, 7], 7),   # Expected Output: [1, 2, 3, 4, 5, 6, 7] (k = len(arr))
+    ([1, 2, 3], 0),               # Expected Output: [1, 2, 3] (k = 0)
+    ([1], 5),                     # Expected Output: [1] (Single element array)
+    ([1, 2], 5),                  # Expected Output: [2, 1] (k > len(arr))
+    ([1, 2, 3, 4], 6),            # Expected Output: [3, 4, 1, 2] (k > len(arr))
+    ([1, 2, 3, 4, 5, 6], 2),      # Expected Output: [5, 6, 1, 2, 3, 4]
+    ([1, 2, 3, 4, 5, 6], 8),      # Expected Output: [5, 6, 1, 2, 3, 4] (k > len(arr))
+    ([1, 2, 3, 4, 5, 6], 10),     # Expected Output: [3, 4, 5, 6, 1, 2] (k > len(arr))
+]
+for testcase in test_cases:
+    print(rotate_array(testcase[0],testcase[1]))

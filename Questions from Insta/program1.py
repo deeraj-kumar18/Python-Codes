7f153028f5d23f0735f2fcdedbaf185ff452d611
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-08-12 19:30:05 +0530

    code first version

--- Questions from Insta/program1.py
+++ Questions from Insta/program1.py
@@ -8,3 +8,19 @@ ex2:
 Input:b3c6d15
 Output:
 '''
+# CODE
+def program1(s):
+    i=0
+    while(i<len(s)):
+        char=s[i]
+        i+=1
+        num_str=""
+        while(i<len(s) and s[i].isdigit()):
+            num_str+=s[i]
+            i+=1
+        
+        if num_str:
+            num=int(num_str)
+            print(char*num,end="")
+
+program1("a5b10")

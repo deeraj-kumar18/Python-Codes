901561652c08041a31e1d84b616e6b3be04768b9
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-08-12 22:26:48 +0530

    multiple testcases

--- DSA/Arrays/Easy/Remove duplicates from Sorted Array.py
+++ DSA/Arrays/Easy/Remove duplicates from Sorted Array.py
@@ -23,5 +23,21 @@ def remove_duplicates(arr):
     return slow+1
 
 # Initial Testing
-arr=[0,0,1,1,1,2,2,3,3,4]
-print(remove_duplicates(arr))
+# arr=[0,0,1,1,1,2,2,3,3,4]
+
+# TESTCASES
+test_cases = [
+    [0, 0, 1, 1, 1, 2, 2, 3, 3, 4],  # Expected Output: 5, array after function call: [0, 1, 2, 3, 4, _, _, _, _, _]
+    [1, 1, 2],  # Expected Output: 2, array after function call: [1, 2, _]
+    [1, 1, 1, 1],  # Expected Output: 1, array after function call: [1, _, _, _]
+    [1, 2, 3, 4, 5],  # Expected Output: 5, array after function call: [1, 2, 3, 4, 5]
+    [0, 0, 0, 0, 0],  # Expected Output: 1, array after function call: [0, _, _, _, _]
+    [1, 1, 2, 3, 3, 4, 4, 5, 5],  # Expected Output: 5, array after function call: [1, 2, 3, 4, 5, _, _, _, _]
+    [2, 2, 2, 2, 2, 2],  # Expected Output: 1, array after function call: [2, _, _, _, _, _]
+    [],  # Expected Output: 0, array after function call: []
+    [3, 3, 3, 3],  # Expected Output: 1, array after function call: [3, _, _, _]
+    [1, 2, 2, 2, 3],  # Expected Output: 3, array after function call: [1, 2, 3, _, _]
+]
+
+for testcase in test_cases:
+    print(remove_duplicates(testcase))

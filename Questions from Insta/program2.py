45a80122b781cbeeb0a7961d53307d763d818f1a
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-08-13 22:01:22 +0530

    universal function for the pattern2

--- Questions from Insta/program2.py
+++ Questions from Insta/program2.py
@@ -16,3 +16,18 @@ for i in range(1,6):
             print((w)%5,end=" ")
         w+=1
     print()
+
+# UNIVERSAL CASE
+def pattern2(n):
+    for i in range(1,n+1):
+        w=i
+        for j in range(1,n+1):
+            if w<(n+1):
+                print(w,end=" ")
+            else:
+                print(w%n,end=" ")
+            w+=1
+        print()
+
+n=int(input("Enter any number:"))
+pattern2(n)

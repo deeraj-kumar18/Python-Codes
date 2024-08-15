56b7aaa216d8567ef7267838e7c9d960cf9f6917
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-08-15 22:54:23 +0530

    initial code

--- DSA/Patterns/patterns.py
+++ DSA/Patterns/patterns.py
@@ -223,7 +223,12 @@ def pattern16(n):
 *  *
 ****
 '''
-
+def pattern21(n):
+    for i in range(n):
+        if (i==0 or i==n-1):
+            print("*"*n)
+        else:
+            print("*" + " "*(n-2) + "*")
 
 def main():
     pattern1(3)

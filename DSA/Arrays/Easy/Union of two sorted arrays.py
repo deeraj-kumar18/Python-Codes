85e72a06afd26dbd4e22fb5c3f911a3f3d214ac0
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-08-16 17:54:54 +0530

    union of two sorted arrays question desc

---
+++ DSA/Arrays/Easy/Union of two sorted arrays.py
@@ -0,0 +1,31 @@
+'''
+Union of Two Sorted Arrays https://www.geeksforgeeks.org/problems/union-of-two-sorted-arrays-1587115621/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=union-of-two-sorted-arrays
+Given two sorted arrays of size n and m respectively, find their union. The Union of two arrays can be defined as the common and distinct elements in the two arrays. Return the elements in sorted order.
+
+Example 1:
+Input: 
+n = 5, arr1[] = {1, 2, 3, 4, 5}  
+m = 5, arr2 [] = {1, 2, 3, 6, 7}
+Output: 
+1 2 3 4 5 6 7
+Explanation: 
+Distinct elements including both the arrays are: 1 2 3 4 5 6 7.
+
+Example 2:
+Input: 
+n = 5, arr1[] = {2, 2, 3, 4, 5}  
+m = 5, arr2[] = {1, 1, 2, 3, 4}
+Output: 
+1 2 3 4 5
+Explanation: 
+Distinct elements including both the arrays are: 1 2 3 4 5.
+
+Example 3:
+Input:
+n = 5, arr1[] = {1, 1, 1, 1, 1}
+m = 5, arr2[] = {2, 2, 2, 2, 2}
+Output: 
+1 2
+Explanation: 
+Distinct elements including both the arrays are: 1 2.
+'''

eed0372a6f726cb9575b41e48d2128c63362f9e4
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-08-20 22:29:33 +0530

    code using split function

--- Questions from Insta/replace all occurances of substring.py
+++ Questions from Insta/replace all occurances of substring.py
@@ -9,7 +9,7 @@ Output : test_str = “abcdforabcd” Explanation : We replace all occurrences o
 Input : test_str = “geeksforgeeks” s1 = “for” s2 = “abcd” 
 Output : test_str = “geeksabcdgeeks”
 '''
-# USING INBUILT FUNCTIONS.
+# USING INBUILT FUNCTION REPLACE.
 def replace(test_Str,s1,s2):
     return test_Str.replace(s1,s2)
 
@@ -17,3 +17,21 @@ test_str = "geeksforgeeks"
 s1 = "geeks"
 s2 = "abcd"
 print(replace(test_str,s1,s2))
+
+# USING SPLIT.
+def replace1(test_str,s1,s2):
+    s=test_str.split(s1)  # splitting the given sentence on the basis of target word.
+    # print(s)  # debug statement.
+    output=""
+    for i in s:
+        if i=="":
+            output+=s2
+        else:
+            output+=i
+
+    return output
+
+test_str = "geeksforgeeks"
+s1 = "geeks"
+s2 = "abcd"
+print(replace1(test_str,s1,s2))

923d494fe6994699381ee34d6871d66622d23ce2
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-08-20 22:23:18 +0530

    replace all occurances question desc

---
+++ Questions from Insta/replace all occurances of substring.py
@@ -0,0 +1,11 @@
+'''
+Python – Replace all occurrences of a substring in a string
+
+Sometimes, we need to replace all occurrences of a substring with other.
+
+Input : test_str = “geeksforgeeks” s1 = “geeks” s2 = “abcd” 
+Output : test_str = “abcdforabcd” Explanation : We replace all occurrences of s1 with s2 in test_str. 
+
+Input : test_str = “geeksforgeeks” s1 = “for” s2 = “abcd” 
+Output : test_str = “geeksabcdgeeks”
+'''

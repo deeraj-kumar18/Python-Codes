bedd844117b71c168bdb6de56d3a1d9efe38a3c2
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-08-20 22:26:18 +0530

    code using inbuilt functions

--- Questions from Insta/replace all occurances of substring.py
+++ Questions from Insta/replace all occurances of substring.py
@@ -9,3 +9,11 @@ Output : test_str = “abcdforabcd” Explanation : We replace all occurrences o
 Input : test_str = “geeksforgeeks” s1 = “for” s2 = “abcd” 
 Output : test_str = “geeksabcdgeeks”
 '''
+# USING INBUILT FUNCTIONS.
+def replace(test_Str,s1,s2):
+    return test_Str.replace(s1,s2)
+
+test_str = "geeksforgeeks"
+s1 = "geeks"
+s2 = "abcd"
+print(replace(test_str,s1,s2))

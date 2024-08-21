a8c20d3adc22bbe81a6562131b48a22e4f77a34f
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-08-21 23:30:58 +0530

    solution using bit manipulation

--- DSA/Arrays/Easy/Single Element.py
+++ DSA/Arrays/Easy/Single Element.py
@@ -15,9 +15,19 @@ Output: 4
 Example 3:
 Input: nums = [1]
 Output: 1
- 
+
 Constraints:
 1 <= nums.length <= 3 * 104
 -3 * 104 <= nums[i] <= 3 * 104
 Each element in the array appears twice except for one element which appears only once.
 '''
+# USING BIT MANIPULATION
+def singleNumber(nums):
+    res=0
+    for i in nums:
+        res=res^i
+
+    return res
+
+nums = [4,1,2,1,2]
+print(singleNumber(nums))

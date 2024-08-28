aca6d9d313be70089af27b131d0b6c1891eabaf1
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-08-28 22:00:01 +0530

    printing all subarrays without using slicing.

--- DSA/Arrays/Easy/Printing all Subarrays.py
+++ DSA/Arrays/Easy/Printing all Subarrays.py
@@ -1,3 +1,4 @@
+# With Slicing
 def printSubarrays(arr):
     for i in range(len(arr)):
         for j in range(i,len(arr)):
@@ -5,3 +6,14 @@ def printSubarrays(arr):
 
 arr=[-1,4,7,2]
 printSubarrays(arr) 
+
+# Without Slicing 
+def printSubarrays1(arr):
+    for start in range(len(arr)):
+        subarray=[]
+        for end in range(start,len(arr)):
+            subarray.append(arr[end])
+            print(subarray)
+
+arr=[1,2,3]
+printSubarrays1(arr)

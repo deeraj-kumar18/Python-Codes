aa2d2f5afa72f0b8948c84a035f59c916246d3da
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-08-28 21:57:46 +0530

    code of printing subarrays using slicing

--- DSA/Arrays/Easy/Printing all Subarrays.py
+++ DSA/Arrays/Easy/Printing all Subarrays.py
@@ -1,12 +1,7 @@
 def printSubarrays(arr):
-        for i in range(len(arr)):
-            print(arr[i])
-            c=len(arr)
-            for j in range(i,len(arr)-c):
-                print(arr[j],end=" ")
-                c+=1
-            print()
-
+    for i in range(len(arr)):
+        for j in range(i,len(arr)):
+            print(arr[i:j+1])
 
 arr=[-1,4,7,2]
-printSubarrays(arr)
+printSubarrays(arr) 

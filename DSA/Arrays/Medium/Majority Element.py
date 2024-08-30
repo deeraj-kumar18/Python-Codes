18d259e9fbccf3ab637768e103da9793f82b45ed
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-08-30 15:07:23 +0530

    Better Approach of Majority element

--- DSA/Arrays/Medium/Majority Element.py
+++ DSA/Arrays/Medium/Majority Element.py
@@ -21,6 +21,7 @@ n == nums.length
 '''
 iterate two loops and count the occurance each time, if occurance is 
 greater than n//2, we return the number.
+Time Complexity: O(n^2), Space Complexity:O(1)
 '''
 def majority_brute(arr):
     for i in range(len(arr)):
@@ -34,3 +35,23 @@ def majority_brute(arr):
 
 nums =[2,2,1,1,1,1,1,2,2,3,3,3,3,3,3,3,3,3,3]
 print(majority_brute(nums))
+
+# Better Approach
+'''
+Use a HashMap, as it is much quicker. 
+Time Complexity: O(N*logN) + O(N), where N = size of the given array.
+Reason: We are using a map data structure. Insertion in the map takes logN time. And we are doing it for N elements. 
+So, it results in the first term O(N*logN). The second O(N) is for checking which element occurs more than floor(N/2) times. 
+If we use unordered_map instead, the first term will be O(N) for the best and average case and for the worst case, it will be O(N2).
+Space Complexity: O(N) as we are using a map data structure.
+'''
+from collections import Counter
+def majority_better(arr):
+    counter=Counter(arr)
+
+    for num,counter in counter.items():
+        if counter>len(arr)//2:
+            return num
+    return -1
+
+print(majority_better(nums))

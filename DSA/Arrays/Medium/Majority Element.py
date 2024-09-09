650bf08083ca1c0c237003648e1c298f29a1f0fa
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-09-09 22:10:40 +0530

    optimal approach of Majority element question.

--- DSA/Arrays/Medium/Majority Element.py
+++ DSA/Arrays/Medium/Majority Element.py
@@ -33,7 +33,9 @@ def majority_brute(arr):
         if count>len(arr)//2:
             return arr[i]
 
-nums =[2,2,1,1,1,1,1,2,2,3,3,3,3,3,3,3,3,3,3]
+nums =[2,2,1,1,1,1,1,1,1,1,1,1,1,2,2,3,3,3,1,3,1,1,1,3,3,3,3,3,3]
+# print(len(nums))
+# print(nums.count(1))
 print(majority_brute(nums))
 
 # Better Approach
@@ -57,3 +59,39 @@ def majority_better(arr):
     return -1   
 
 print(majority_better(nums))
+
+# Optimal Approach
+'''
+Using Boyer-Moore Majority Vote Algorithm, which is an efficient way to find the majority element in a single pass through the list.
+
+Approach
+1.We'll use two variables, "count" and "majority," to keep track of the current majority candidate and its count.
+2.We'll start by assuming the first element in the list as our "majority candidate" and set the count to 1.
+3.As we traverse through the list, we'll compare each element with the current majority candidate.
+4.If the current element is the same as the majority candidate, we'll increment the count as we've found another occurrence of this element.
+5.If the current element is different from the majority candidate, we'll decrement the count as it's like "canceling out" one occurrence of different elements.
+6.Whenever the count becomes 0, we'll update the majority candidate to the current element and reset the count to 1. This step ensures that we always have a majority candidate at any point in the loop.
+7.By the end of this process, the majority candidate will hold the element that appears more than half of the time in the list.
+
+Complexity
+Time complexity:
+The Boyer-Moore Majority Vote Algorithm goes through the list once, comparing and updating elements in constant time. Thus, the time complexity is O(N), where N is the size of the input list.
+Space complexity:
+The algorithm uses only a constant amount of additional space for storing variables. Hence, the space complexity is O(1).
+'''
+def majority_optimal(nums):
+    count=0
+    majority_element=0
+
+    for i in range(len(nums)):
+        if count==0 and majority_element!=nums[i]:
+            majority_element=nums[i]
+            count+=1
+        elif majority_element==nums[i]:
+            count+=1
+        else:
+            count-=1
+    
+    return majority_element
+
+print(majority_optimal(nums))

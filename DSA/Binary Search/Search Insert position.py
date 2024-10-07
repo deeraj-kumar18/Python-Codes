a4f913a71853b5d8e22fea1ca19c8df27206590a
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-10-07 23:56:12 +0530

    code

--- DSA/Binary Search/Search Insert position.py
+++ DSA/Binary Search/Search Insert position.py
@@ -23,3 +23,21 @@ Constraints:
 nums contains distinct values sorted in ascending order.
 -104 <= target <= 104
 '''
+
+def search_position(nums,n,target):
+    low,high=0,n-1
+    ans=n
+    while low<=high:
+        mid=(low+high)//2
+        if nums[mid]>=target:
+            ans=mid
+            high=mid-1
+        
+        else:
+            low=mid+1
+    
+    return ans
+
+nums = [1,3,5,6]
+target = 2
+print(search_position(nums,len(nums),target))

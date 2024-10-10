27da7809e464192f7b118eb728ccae78c6096e59
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-10-11 00:38:23 +0530

    Search in Rotated Sorted Array II optimal solution

--- DSA/Binary Search/Search in Rotated Sorted Array II.py
+++ DSA/Binary Search/Search in Rotated Sorted Array II.py
@@ -24,3 +24,43 @@ Constraints:
 nums is guaranteed to be rotated at some pivot.
 -104 <= target <= 104
 '''
+# Approach
+'''
+The major issue arises when the duplicates are in low, mid and high positions at the same time. At this
+condition our previous code breaks. Hence we need to resolve the condition 
+arr[low] == arr[mid] == arr[high]
+WE shrink our search space by one element each on left and right side. 
+'''
+def search_binary(nums,target):
+    low,high=0,len(nums)-1
+    while low<=high:
+        mid=(low+high)//2
+        if nums[mid]==target:
+            return True
+        
+        # Condition
+        if nums[low]==nums[mid]==nums[high]:
+            low+=1
+            high-=1
+            continue
+        # Finding for Sorted Half.
+        # Left Half Sorted Array 
+        if nums[low]<=nums[mid]:
+            # Eliminatimg the search space by comparing values of low, mid and high
+            if nums[low]<=target and target<=nums[mid]:
+                high=mid-1
+            else:
+                low=mid+1
+        
+        # Right Half Sorted Array
+        if nums[mid]<=nums[high]:
+            if nums[mid]<=target and target<=nums[high]:
+                low=mid+1
+            else:
+                high=mid-1
+
+    return False
+
+nums = [2,5,6,0,0,1,2]
+target = 3
+print(search_binary(nums,target))

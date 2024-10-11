12ce085d3b7dcc5d81f5c02a3305d9db92389e69
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-10-11 22:16:32 +0530

    Optimal Approach

--- DSA/Binary Search/Single Element in Sorted Array.py
+++ DSA/Binary Search/Single Element in Sorted Array.py
@@ -54,11 +54,51 @@ TC: O(n)
 SC: O(n)
 '''
 from collections import Counter
-def singleNonDuplicate(nums):
+def singleNonDuplicate1(nums):
     dict=Counter(nums)
     for key,val in dict.items():
         if val==1:
             return key
 
 nums = [1,1,2,3,3,4,4,8,8]
-print(singleNonDuplicate(nums))
+print(singleNonDuplicate1(nums))
+
+# Optimal Approach 
+
+def singleNonDuplicate(arr):
+    n = len(arr)  # Size of the array
+
+    # Edge cases:
+    if n == 1:
+        return arr[0]
+    if arr[0] != arr[1]:
+        return arr[0]
+    if arr[n - 1] != arr[n - 2]:
+        return arr[n - 1]
+
+    low = 1
+    high = n - 2
+    while low <= high:
+        mid = (low + high) // 2
+
+        # If arr[mid] is the single element:
+        if arr[mid] != arr[mid + 1] and arr[mid] != arr[mid - 1]:
+            return arr[mid]
+
+        # We are in the left:
+        if (mid % 2 == 1 and arr[mid] == arr[mid - 1]) or (mid % 2 == 0 and arr[mid] == arr[mid + 1]):
+            # Eliminate the left half:
+            low = mid + 1
+        # We are in the right:
+        else:
+            # Eliminate the right half:
+            high = mid - 1
+
+    # Dummy return statement:
+    return -1
+
+arr = [1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6]
+ans = singleNonDuplicate(arr)
+print("The single element is:", ans)
+
+

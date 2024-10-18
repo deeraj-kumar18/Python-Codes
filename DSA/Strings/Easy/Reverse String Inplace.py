a3beb9a7e351d457811716ce3fbc9e94e01b52c6
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-10-18 22:54:01 +0530

    Approach using inbuilt Reverse function

--- DSA/Strings/Easy/Reverse String Inplace.py
+++ DSA/Strings/Easy/Reverse String Inplace.py
@@ -24,8 +24,15 @@ def reverseString(s):
     n=len(s)
     for i in range(n//2):
         s[i],s[n-i-1]=s[n-i-1],s[i]
-    
+        
     return s
 
 s = ["h","e","l","l","o"]
 print(reverseString(s))
+
+# Approach using inbuilt Reverse function
+def reverseString1(s):
+    s.reverse()
+    return s
+s1 = ["h","e","l","l","o"]
+print(reverseString1(s1))

70432100a51848f650a6d1e9cb2114ad21777472
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-10-18 22:07:44 +0530

    added documentation

--- DSA/Strings/Zigzag Conversion.py
+++ DSA/Strings/Zigzag Conversion.py
@@ -35,25 +35,34 @@ Constraints:
 s consists of English letters (lower-case and upper-case), ',' and '.'.
 1 <= numRows <= 1000
 '''
+# Approach
+"""
+if numRows is equal to 1, we return the same string.
+we keep two pointers for direction and row_indicator. We flip the direction variable based on the direction of appending as per zigzag condition.
+We append the characters one by one in the rows based on the direction.
+"""
 def convert(s,numRows):
+    
+    # if numRows is equal to 1, we return the same string.
     if numRows==1 or numRows>len(s):
         return s
     
-    direction=1
-    row_indicator=0
+    direction=1  # Used to indicate the direction. 1 for Downward traversal. -1 for Upward traversal.
+    row_indicator=0 # Indicator to append in which row.
 
-    rows=[[] for _ in range(numRows)]
+    rows=[[] for _ in range(numRows)] # 2D List to store the answer.
 
     for char in s:
-        rows[row_indicator].append(char)
+        rows[row_indicator].append(char)  
 
-        if row_indicator==0:
+        if row_indicator==0: # If we come to first row, we change the direction to 1
             direction=1
-        elif row_indicator == numRows-1:
+        elif row_indicator == numRows-1: # If we come to the last row, we change the direction to -1
             direction=-1
         
-        row_indicator+=direction
+        row_indicator+=direction # we update the row until the first or last row is encountered.
     
+    # Parsing the 2D list into string format.
     ans=""
     for row in rows:
         ans+="".join(row)

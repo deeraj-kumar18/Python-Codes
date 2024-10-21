172c96f88c5cf232e8f3c6e815cc93460505c89c
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-10-22 00:01:52 +0530

    Code

--- DSA/Arrays/Hard/Largest Rectangle in Histogram.py
+++ DSA/Arrays/Hard/Largest Rectangle in Histogram.py
@@ -18,3 +18,27 @@ Constraints:
 1 <= heights.length <= 105
 0 <= heights[i] <= 104
 '''
+def largestRectangleArea(heights):
+    max_area=0
+    for i in range(len(heights)):
+        height=heights[i]
+        left,right,=i,i
+
+        while left>0 and heights[left-1]>=height:
+            left-=1
+        
+        while right<len(heights)-1 and heights[right+1]>=height:
+            right+=1
+        
+        width=right-left+1
+
+        area= width*height
+
+        if area>=max_area:
+            max_area=area
+    
+    return max_area
+
+testcases=[[2, 1, 5, 6, 2, 3],[2, 4],[6, 2, 5, 4, 5, 1, 6],[1, 2, 3, 4, 5]]
+for testcase in testcases:
+    print(largestRectangleArea(testcase))

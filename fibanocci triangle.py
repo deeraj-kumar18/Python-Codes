d1698eb4b56e4132d9f383361d1b129d54d8fd87
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-10-22 14:46:00 +0530

    solution

--- fibanocci triangle.py
+++ fibanocci triangle.py
@@ -32,3 +32,28 @@ Explanation:
 - The third row contains three Fibonacci numbers: [2, 3, 5]
 - The fourth row contains four Fibonacci numbers: [8, 13, 21, 34]
 '''
+def printing(lst,n):
+    output=[]
+    c=0
+    for i in range(1,n+1):
+        inner=[]
+        for j in range(i):
+            inner.append(lst[c])
+            c+=1
+        output.append(inner)
+    return output
+
+def fibanocci_pattern(n):
+    count=(n*(n+1))//2
+
+    l=[0,1,1]
+    a,b=1,1
+    for i in range(count-3):
+        a,b=b,a+b
+        l.append(b)
+    
+    a=printing(l,n)
+    return a
+
+n=5
+print(fibanocci_pattern(n))

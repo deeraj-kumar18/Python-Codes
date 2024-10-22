068989e2609941caba6c476815b29ccf046a353f
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-10-22 22:18:55 +0530

    code with comments.

--- DSA/Arrays/Easy/Group Consecutive Numbers.py
+++ DSA/Arrays/Easy/Group Consecutive Numbers.py
@@ -24,3 +24,28 @@ Output:
 Explanation:
 The consecutive '1's are grouped together, the consecutive '2's are grouped together, '3' stands
 alone, and the consecutive '4's are grouped together.'''
+def group_consecutive(lst):
+    output=[]
+    if not lst:
+        return output
+
+    var=lst[0]
+    innerlist=[var]  # Start with the first element in a group
+
+
+    for i in range(1,len(lst)):
+        if lst[i]==var:
+            innerlist.append(lst[i])   # Add to the current group
+        else:
+            output.append(innerlist)    # Save the current group to the result
+            var=lst[i]
+            innerlist=[var] # Start a new group
+    
+    if innerlist:
+        output.append(innerlist) # Don't forget to add the last group
+    
+    return output
+
+lst = [1, 1, 2, 2, 2, 3, 4, 4]
+print(group_consecutive(lst))
+

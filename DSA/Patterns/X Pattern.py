861d145706b31aa4f602fef92d2e640940b38650
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-10-23 12:35:33 +0530

    X pattern question desc

---
+++ DSA/Patterns/X Pattern.py
@@ -0,0 +1,24 @@
+'''
+Problem: Generate an X Shape Pattern with Numbers
+
+Write a program that takes an integer input n and prints an X shape pattern using numbers. The
+numbers decrease as they move towards the center of the X, and then increase as they move away
+from the center.
+
+Input Format:
+- A single integer n where 1 <= n <= 100.
+
+Output Format:
+- The X shape pattern of numbers as described.
+
+Example:
+Input:
+5
+
+Output:
+5       5 
+  4   4   
+    3     
+  2   2   
+1       1 
+'''

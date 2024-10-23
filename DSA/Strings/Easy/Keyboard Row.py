1db7fd106af8270b833db83dacb6082d43818111
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-10-23 22:07:59 +0530

    code

--- DSA/Strings/Easy/Keyboard Row.py
+++ DSA/Strings/Easy/Keyboard Row.py
@@ -27,3 +27,33 @@ Constraints:
 1 <= words[i].length <= 100
 words[i] consists of English letters (both lowercase and uppercase). 
 '''
+# Own Approach 
+def find_words(words):
+    firstrow='qwertyuiopQWERTYUIOP'
+    secondrow='asdfghjklASDFGHJKL'
+    thirdrow='zxcvbnmZXCVBNM'
+    output=[]
+
+    for word in words:
+        firstrowFlag=True
+        secondrowFlag=True
+        thirdrowFlag=True
+        for char in word:
+            if char not in firstrow:
+                firstrowFlag=False
+            if char not in secondrow:
+                secondrowFlag=False
+            if char not in thirdrow:
+                thirdrowFlag=False
+        
+        if firstrowFlag:
+            output.append(word)
+        if secondrowFlag:
+            output.append(word)
+        if thirdrowFlag:
+            output.append(word)
+        
+    return output
+
+input=["Hello", "Alaska", "Dad", "Peace"]
+print(find_words(input))

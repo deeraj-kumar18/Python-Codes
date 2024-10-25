158753522c223ea8f83f8fdb05988cf3a06ab6d2
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-10-25 21:02:13 +0530

    code

--- DSA/Strings/Medium/Multiply Strings GFG.py
+++ DSA/Strings/Medium/Multiply Strings GFG.py
@@ -27,3 +27,32 @@ Expected Auxiliary Space: O(n1 + n2); where n1 and n2 are sizes of strings s1 an
 Constraints:
 1 ≤ length of s1 and s2 ≤ 103
 '''
+# Own Approach
+def multiplyStrings(s1,s2):
+    # return the product string
+    dic={'0':0,'1':1,'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9}
+    no1=0
+    j=1
+    for i in range(len(s1)-1,-1,-1):
+        if s1[i] in dic:
+            no1+=dic[s1[i]]*j
+            j*=10
+            
+    if s1[0]=="-":
+        no1*=-1
+        
+    no2=0
+    j=1
+    for i in range(len(s2)-1,-1,-1):
+        if s2[i] in dic:
+            no2+=dic[s2[i]]*j
+            j*=10
+    
+    if s2[0]=="-":
+        no2*=-1
+        
+    return no1*no2
+
+s1 = "0033"
+s2 = "2"
+print(multiplyStrings(s1,s2))

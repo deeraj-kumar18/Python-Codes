f328fa873a3d929c7cf6f2cefecd2ba8a3105348
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-10-25 20:59:05 +0530

    multiply strings GFG question description

---
+++ DSA/Strings/Medium/Multiply Strings GFG.py
@@ -0,0 +1,29 @@
+'''
+Multiply two strings        https://www.geeksforgeeks.org/problems/multiply-two-strings/1?page=2&sortBy=submissions
+
+Difficulty: Medium
+
+Given two numbers as strings s1 and s2. Calculate their Product.
+
+Note: The numbers can be negative and You are not allowed to use any built-in function or convert the strings to integers. There can be zeros in the begining of the numbers. You don't need to specify '+' sign in the begining of positive numbers.
+
+Example 1:
+Input:
+s1 = "0033"
+s2 = "2"
+Output:
+66
+
+Example 2:
+Input:
+s1 = "11"
+s2 = "23"
+Output:
+253
+
+Expected Time Complexity: O(n1* n2)
+Expected Auxiliary Space: O(n1 + n2); where n1 and n2 are sizes of strings s1 and s2 respectively.
+
+Constraints:
+1 ≤ length of s1 and s2 ≤ 103
+'''

4146bbc5024dad88c26843d8a813c39f330c9f76
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-10-26 20:03:33 +0530

    comments

--- DSA/Strings/Easy/Valid Anagram.py
+++ DSA/Strings/Easy/Valid Anagram.py
@@ -18,9 +18,11 @@ s and t consist of lowercase English letters.
 '''
 from collections import Counter
 def isAnagram(s, t):
+    # Creating dictionaries with frequencies of characters in the string.
     d1=Counter(s)
     d2=Counter(t)
 
+    # If the frequencies of characters are same in the two dictionaries, we return True else False.
     return d1==d2
 
 s = "anagram"

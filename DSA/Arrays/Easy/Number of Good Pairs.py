e78daa4ac43689c7b15479a957bedaacfef3c6fb
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-10-31 13:36:31 +0530

    bruteforce approach code

--- DSA/Arrays/Easy/Number of Good Pairs.py
+++ DSA/Arrays/Easy/Number of Good Pairs.py
@@ -25,3 +25,16 @@ Constraints:
 1 <= nums.length <= 100
 1 <= nums[i] <= 100
 '''
+# BruteForce Approach
+def numIdenticalPairs(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    count=0
+    for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            if nums[i]==nums[j]:
+                count+=1
+
+    return count

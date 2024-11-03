af04c8d8c279550081aa59cd692ce51bd7520c78
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-11-03 12:41:20 +0530

    solution using stack

--- DSA/Strings/Easy/Valid Parentheses.py
+++ DSA/Strings/Easy/Valid Parentheses.py
@@ -31,3 +31,27 @@ Constraints:
 1 <= s.length <= 104
 s consists of parentheses only '()[]{}'.
 '''
+# Approach using Stack
+'''
+1) We store the closing brackets as keys and opening brackets as values in a dictionary.
+2) Iterate over the string and if the character is not a closing bracket, we append it to our stack.
+3) If it is a closing bracket and if the stack is not empty, we pop the top most element and check if it is the same as value for that character in dictionary. 
+4)If they are not equal we return false.
+5) At the end, if the stack is empty, we return true else False.
+'''
+def isValid(s):
+    ans=[]
+    dict={")":"(" , "}":"{" , "]":"["}
+
+    for char in s:
+        if char in dict and ans:
+            if dict[char]!=ans.pop():
+                return False
+        else:
+            ans.append(char)
+    
+    return True if not ans else False 
+
+testcases=["()","()[]{}","(]","([])","[","(){}}{"]
+for testcase in testcases:
+    print(isValid(testcase))

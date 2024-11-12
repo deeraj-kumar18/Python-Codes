4da9faf5f7db6f2f9e2b0abb2922433345449a6b
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-11-12 09:41:53 +0530

    Bruteforce approach

--- DSA/Strings/Easy/Check if the Sentence Is Pangram.py
+++ DSA/Strings/Easy/Check if the Sentence Is Pangram.py
@@ -20,3 +20,30 @@ Constraints:
 1 <= sentence.length <= 1000
 sentence consists of lowercase English letters.
 '''
+# Approach 1 using For Loop
+'''
+TC: O(N)
+SC: O(1)
+'''
+def checkIfPangram(sentence):
+    """
+    :type sentence: str
+    :rtype: bool
+    """
+    if len(sentence)<26:
+        return False
+
+    alpha="qwertyuiopasdfghjklzxcvbnm"
+    c=0
+    for i in alpha:
+        if i in sentence:
+            c+=1
+    
+    if c==26:
+        return True
+    else:
+        return False
+
+testcases=["thequickbrownfoxjumpsoverthelazydog","leetcode","jwtucoucmdfwxxqnxzkaxoglszmfrcvjoiunqqausaxxaaijyqdqgvdnqcaihwilqkpivenpnekioyqujrdrovqrlxovcucjqzjsxmllfgndfprctxvxwlzjtciqxgsxfwhmuzqvlksyuztoetyjugmswfjtawwaqmwyxmvo"]
+for testcase in testcases:
+    print(checkIfPangram(testcase))

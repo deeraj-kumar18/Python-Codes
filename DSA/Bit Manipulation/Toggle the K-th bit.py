c49e7457a230f523bfecc19f323d26ad37dbc3b3
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-11-13 19:58:57 +0530

    comments

--- DSA/Bit Manipulation/Toggle the K-th bit.py
+++ DSA/Bit Manipulation/Toggle the K-th bit.py
@@ -4,6 +4,7 @@ Given a number and an index k, toggle the bit at that index.
 1 -> 0
 """
 def toggleBit(n,k):
+    # We left shift 1 by k times and perform XOR operation with number, the bit is toggled.
     return n ^ (1<<k)
 
 print(toggleBit(13,1))

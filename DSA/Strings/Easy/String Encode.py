0b2b027bb283d6601ffd1d95deb9a9c867656a47
deeraj-kumar18 <dheerajkumarvirat18@msitporgram.net>
2024-11-18 17:57:46 +0530

    String Encode question description

---
+++ DSA/Strings/Easy/String Encode.py
@@ -0,0 +1,8 @@
+'''
+String Encode
+
+Given a string, compress it using the counts of repeated characters. 
+
+For example, the input aaaabbbbbbcccdd should be encoded as 4a6b3c2d, 
+where each sequence of repeated characters is replaced by its count followed by the character.
+'''
